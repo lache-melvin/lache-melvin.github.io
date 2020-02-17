ee668555fc18770955bc020f2a7b70ca7a0b6041
Lache Melvin <lache.melvin@gmail.com>
2020-02-17 19:47:22 +1300

    remove example js file

--- g.js
+++
@@ -1,9 +0,0 @@
-
-
-function isATreehugger(caresAboutClimateChange, Name) {
-    if (caresAboutClimateChange) {
-        return "Yes, " + Name + " is a treehugger, and should be proud of it."
-    } else {
-        return "No, " + Name + " should probably do some research."
-    }
-}
