69106c20c717b91e7a66d9ad8d3a6e28c6929f31
Lache Melvin <lache.melvin@gmail.com>
2020-02-17 19:46:38 +1300

    add functions explanation

---
+++ g.js
@@ -0,0 +1,9 @@
+
+
+function isATreehugger(caresAboutClimateChange, Name) {
+    if (caresAboutClimateChange) {
+        return "Yes, " + Name + " is a treehugger, and should be proud of it."
+    } else {
+        return "No, " + Name + " should probably do some research."
+    }
+}
